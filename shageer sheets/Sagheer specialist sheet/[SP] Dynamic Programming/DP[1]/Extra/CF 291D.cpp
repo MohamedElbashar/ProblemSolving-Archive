7fc290c5f0fcfd4e611c84bef9fee790c08fbbaa
Mohamed Elbashar <mohamedbashar200@gmail.com>
2017-09-28 11:25:50 +0200

    init

---
+++ shageer sheets/Sagheer specialist sheet/[SP] Dynamic Programming/DP[1]/Extra/CF 291D.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef vector<pi> vpi;
+#define EPS 1e-9
+#define PI 3.1415926535897932384626433832795
+#define pb push_back
+#define mp make_pair
+#define all(v) v.begin(),v.end()
+#define mem(a,b) memset(a,b,sizeof a)
+#define sz(v) ((int)((v).size()))
+#define minn(a, b, c) min(min(a, b), c)
+#define maxx(a, b, c) max(max(a, b), c)and
+int dx[] = { 1, 1, 0, -1, -1, -1, 0, 1 };
+int dy[] = { 0, 1, 1, 1, 0, -1, -1, -1 };
+int di[] = { 1, -1, 0, 0 };
+int dj[] = { 0, 0, 1, -1 };
+void file() {
+#ifndef ONLINE_JUDGE
+	freopen("in.txt", "r", stdin);
+	//freopen("out.txt", "w", stdout);
+#else
+	// online submission
+	//freopen("sets.in", "r", stdin);
+#endif
+}
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\//\/\/\/\/\/\/\/\/\/\/
+int n, k;
+int arr[10001];
+int main() {
+	file();
+	scanf("%d%d", &n, &k);
+	for (int i = 1; i < n; i++) {
+		arr[i] = 1;
+	}
+	for (int i = 0; i < k; i++) {
+		for (int j = 1; j <= n; j++) {
+			printf("%d ", arr[j] + j);
+			arr[j] += arr[arr[j] + j];
+		}
+		printf("\n");
+	}
+}
