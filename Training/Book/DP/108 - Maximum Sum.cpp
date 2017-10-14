b9eb53d3f4cbf7d0e4d0d37e10c6d2beee904242
Mohamed Elbashar <mohamedbashar200@gmail.com>
2017-10-14 22:45:38 +0200

    init

--- Training/Book/DP/108 - Maximum Sum.cpp
+++ Training/Book/DP/108 - Maximum Sum.cpp
@@ -29,7 +29,6 @@ int main() {
 	file();
 	int n;
 	while (scanf("%d", &n) != EOF) {
-
 		for (int i = 1; i <= n; i++)
 			for (int j = 1; j <= n; j++) {
 				scanf("%d", &arr[i][j]);
---
+++ shageer sheets/Sagheer specialist sheet/[SP] Dynamic Programming/DP[2]/Book Examples/108 - Maximum Sum [bottom-up].cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+typedef vector<vector<int> > vvi;
+typedef vector<pi> vpi;
+#define EPS 1e-9
+#define PI 3.1415926535897932384626433832795
+#define pb push_back
+#define mp make_pair
+#define all(v) v.begin(),v.end()
+#define mem(a,b) memset(a,b,sizeof a)
+#define sz(v) ((int)((v).size()))
+#define minn(a, b, c) min(min(a, b), c)
+#define maxx(a, b, c) max(max(a, b), c)
+int dx[] = { 1, 1, 0, -1, -1, -1, 0, 1 };
+int dy[] = { 0, 1, 1, 1, 0, -1, -1, -1 };
+int di[] = { 1, -1, 0, 0 };
+int dj[] = { 0, 0, 1, -1 };
+void file() {
+#ifndef ONLINE_JUDGE
+	freopen("in.txt", "r", stdin);
+	//freopen("out.txt", "w", stdout);
+#else
+	// online submission
+	//freopen("sets.in", "r", stdin);
+#endif
+}
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\//\/\/\/\/\/\/\/\/\/\/
+int main() {
+	file();
+}
