a2041de65674522bb01e7e6715dbdf000442cc28
Mohamed Elbashar <mohamedbashar200@gmail.com>
2017-10-19 18:41:17 +0200

    init

--- shageer sheets/Sagheer specialist sheet/[SP] Dynamic Programming/DP[2]/Book Examples/108 - Maximum Sum [bottom-up].cpp
+++ shageer sheets/Sagheer specialist sheet/[SP] Dynamic Programming/DP[2]/Book Examples/108 - Maximum Sum [bottom-up].cpp
@@ -28,6 +28,35 @@ void file() {
 #endif
 }
 //\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\//\/\/\/\/\/\/\/\/\/\/
+int A[100][100];
 int main() {
-	file();
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < n; j++) {
+			scanf("%d", &A[i][j]);
+			if (i > 0)
+				A[i][j] += A[i - 1][j];
+			if (j > 0)
+				A[i][j] += A[i][j - 1];
+			if (i > 0 && j > 0)
+				A[i][j] -= A[i - 1][j - 1];
+		}
+
+	maxSubRect = -1e9;
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < n; j++)
+			for (int k = i; k < n; k++)
+				for (int l = j; l < n; l++) {
+					subRect = A[k][l];
+					if (i > 0)
+						subRect -= A[i - 1][l];
+					if (j > 0)
+						subRect -= A[k][j - 1];
+					if (i > 0 && j > 0)
+						subRect += A[i - 1][j - 1];
+					maxSubRect = max(maxSubRect, subRect);
+				}
+
+	printf("%d\n", maxSubRect);
+	return 0;
 }
